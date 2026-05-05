00cdd50b9775c62252f6d8fe94ef267c904b9b8d
JaimeGar99-del <garciajaimejrgc9999@gmail.com>
2026-05-05 10:14:57 -0500

    promediojs

--- base.js
+++ base.js
@@ -91,10 +91,136 @@
 // for (const tag of tags) {
 //     console.log(tag);
 // }
-const setA = new Set([1, 2, 3, 4, 5]);
-const setB = new Set([3, 4, 5, 6, 7]);
-const arrayA = [...setA]
-console.log("setA",...arrayA);
-console.log("setB",...setB );
-const interseccion = arrayA.filter(numero => setB.has(numero));
-console.log("numero en ambos:",...interseccion)
+// const setA = new Set([1, 2, 3, 4, 5]);
+// const setB = new Set([3, 4, 5, 6, 7]);
+// const arrayA = [...setA]
+// console.log("setA",...arrayA);
+// console.log("setB",...setB );
+// const interseccion = arrayA.filter(numero => setB.has(numero));
+// console.log("numero en ambos:",...interseccion);
+
+// const setA = new Set([1, 2, 3, 4, 5]);
+// const setB = new Set([3, 4, 5, 6, 7]);
+
+// const interseccion = new Set();
+
+// for(const num of setA){
+//     if(setB.has(num)){
+//         interseccion.add(num);
+//     }
+// }
+// console.log("setA:", ...setA);
+// console.log("setB:", ...setB);
+// console.log("numeros en comun:", ...interseccion);
+
+// const palabras = ["javascript", "python", "javascript", "java", "python", "javascript"];
+
+// const counter = new Map();
+
+// for(const word of palabras){
+//     const conteoActual = counter.get(word) || 0;
+//     counter.set(word, conteoActual+1)
+// }
+// counter.forEach((conteo, word) =>{
+//     console.log(word, conteo);
+// });
+
+// const catalogo = new Map();
+
+// catalogo.set("electronica", ["Laptop", "Teclado", "Mouse"]);
+// catalogo.set("ropa", ["Camiseta", "Pantalon"]);
+// catalogo.set("comida", ["Arroz", "Pasta", "Atun"]);
+// catalogo.get("electronica").length
+// catalogo.get("ropa").push("zapatos")
+// catalogo.forEach((productos, categoria) =>{
+//     console.log(categoria,":", ...productos)
+// });
+// catalogo.forEach((productos, categoria) =>{
+//     console.log("--- " + categoria + " ---");
+//     for(const producto of productos){
+//         console.log(producto);
+//     }
+// });
+// function validarProducto(producto) {
+//     if (!producto.id) {
+//         return "ERROR: falta el id";
+//     }
+//     if (!producto.nombre || producto.nombre.trim() === "") {
+//         return "Error: el nombre no es valido";
+//     }
+//     if (typeof producto.precio !== "number" || producto.precio <= 0) {
+//         return "Error: el precio no es valido";
+//     }
+//     return `id: ${producto.id} | nombre: ${producto.nombre} | precio: ${producto.precio}`;
+// }
+
+// console.log(validarProducto({ id: 1, nombre: "Laptop", precio: 500000 }));
+// console.log(validarProducto({ nombre: "Laptop", precio: 500000 }));
+// console.log(validarProducto({ id: 2, nombre: "   ", precio: 500000 }));
+// console.log(validarProducto({ id: 3, nombre: "Mouse", precio: -100 }));
+
+/* crear un calculador de notas por coder (especificar aprobado y reprobado) 
+calcular promedio mas alto 
+requisitos: 
+usar funcion calcular promedio 
+usar operador ternario
+*/
+// const coder = ["janner","sharick", "luigui", "jaime", "isaac", "daniel", "adriano"];
+// const notas = [[5,3,4],[5,4,4],[5,2,4],[5,2,4],[5,5,4],[2,3,4],[3,2,3]];
+
+
+// for (let indice = 0; indice < coder.length; indice++) {
+//     console.log(`Coder: ${coder[indice]}`);
+//     console.log(`Notas: ${notas[indice]}`); 
+//     let suma = 0;
+//     for (let nota of notas[indice]) {
+//         suma += nota;
+        
+//     }
+//     console.log(suma);
+//     let promedioMasAlto = 0;
+//     let mejorCoder = "";
+
+//     for (let i = 0; i < coder.length; i++) {
+//         let promedio = calcularPromedio(notas[i]);
+// }
+
+// }
+
+// function calcularpromedio(){
+    
+// }
+// calcularpromedio();
+
+const coder = ["janner","sharick", "luigui", "jaime", "isaac", "daniel", "adriano"];
+const notas = [[5,3,4],[5,4,4],[5,2,4],[5,2,4],[5,5,4],[2,3,4],[3,2,3]];
+
+let promedioMasAlto = 0;
+let mejorCoder = "";
+
+for (let indice = 0; indice < coder.length; indice++) {
+    console.log(`Coder: ${coder[indice]}`);
+    console.log(`Notas: ${notas[indice]}`); 
+
+    let promedioActual = calcularPromedio(notas[indice]);
+    let estado = promedioActual >= 3 ? "Aprobado" : "Reprobado";
+    
+    console.log(`Promedio: ${promedioActual} - Estado: ${estado}`);
+    console.log("------------------------");
+
+    if (promedioActual > promedioMasAlto) {
+        promedioMasAlto = promedioActual;
+        mejorCoder = coder[indice];
+    }
+}
+
+function calcularPromedio(arrayDeNotas) {
+    let suma = 0;
+    for (let nota of arrayDeNotas) {
+        suma += nota;
+    }
+    let resultado = suma / arrayDeNotas.length;
+    return Math.round(resultado *100)/100;
+}
+
+console.log(`EL mejor es ${mejorCoder} con un promedio de ${promedioMasAlto}`);
